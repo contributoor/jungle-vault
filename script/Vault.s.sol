7ba2b8f4b5ceaf2606f22ed2d17fe26d59674a1d
contributoor <contributoor@protonmail.com>
2023-10-11 01:24:34 +0800

    Fix import

--- script/Vault.s.sol
+++ script/Vault.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, console2} from "../lib/forge-std/src/Test.sol";
+import {Script, console2} from "../lib/forge-std/src/Script.sol";
 
 contract VaultScript is Script {
 // TODO
