1f687ec7289fceb39e2faf0ead1c16bae8f4b59b
contributoor <contributoor@protonmail.com>
2023-10-11 01:20:53 +0800

    Init Vault with YieldStrategy

--- src/Vault.sol
+++ src/Vault.sol
@@ -8,23 +8,25 @@ import {ERC20} from "lib/solmate/src/tokens/ERC20.sol";
 import {YieldStrategy} from "./interfaces/YieldStrategy.sol";
 
 contract Vault is ERC4626, Auth {
-    YieldStrategy public currentStrategy;
+    YieldStrategy public activeStrategy;
 
-    constructor(ERC20 _asset)
+    constructor(ERC20 _asset, YieldStrategy _initialStrategy)
         ERC4626(
             _asset,
             string(abi.encodePacked("Jungle ", _asset.name(), "Vault")),
             string(abi.encodePacked("jv", _asset.symbol()))
         )
         Auth(Auth(msg.sender).owner(), Auth(msg.sender).authority())
-    {}
+    {
+        activeStrategy = _initialStrategy;
+    }
 
     receive() external payable {}
 
     function setStrategy(YieldStrategy newStrategy) external requiresAuth {
-        uint256 balance = currentStrategy.withdrawAll();
+        uint256 balance = activeStrategy.withdrawAll();
         newStrategy.deposit(balance);
-        currentStrategy = newStrategy;
+        activeStrategy = newStrategy;
     }
 
     function totalAssets() public view override returns (uint256) {
@@ -32,10 +34,10 @@ contract Vault is ERC4626, Auth {
     }
 
     function afterDeposit(uint256 assets, uint256 /* shares */ ) internal override {
-        currentStrategy.deposit(assets);
+        activeStrategy.deposit(assets);
     }
 
     function beforeWithdraw(uint256 assets, uint256 shares) internal override {
-        currentStrategy.withdraw(assets, shares, totalSupply);
+        activeStrategy.withdraw(assets, shares, totalSupply);
     }
 }
